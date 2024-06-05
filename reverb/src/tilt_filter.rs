2a23f5b92dc58afe28a99bf64de46f4375eb190f
Dave Mollen <dave.mollen@infi.nl>
2024-06-05 20:58:00 +0200

    Update tilt_filter.rs

--- reverb/src/tilt_filter.rs
+++ reverb/src/tilt_filter.rs
@@ -3,19 +3,30 @@ use biquad_filter::BiquadFilter;
 mod bilinear_transform;
 use bilinear_transform::BilinearTransform;
 
+const C1: f32 = 5.6e-9;
+const C2: f32 = 5.6e-9;
+const R1: f32 = 2250.;
+const R2: f32 = 2250.;
+const RF1: f32 = 47000.;
+const RF2: f32 = 47000.;
+const R_TILT: f32 = 140000.;
+
 pub struct TiltFilter {
   bilinear_transform: BilinearTransform,
   biquad_filter: BiquadFilter,
 }
 
 impl TiltFilter {
-  const C1: f32 = 5.6e-9;
-  const C2: f32 = 5.6e-9;
-  const R1: f32 = 2250.;
-  const R2: f32 = 2250.;
-  const RF1: f32 = 47000.;
-  const RF2: f32 = 47000.;
-  const R_TILT: f32 = 140000.;
+  const C1C2: f32 = C1 * C2;
+  const C1C2R1: f32 = Self::C1C2 * R1;
+  const C1C2R1R2: f32 = Self::C1C2R1 * R2;
+  const C1C2R2: f32 = Self::C1C2 * R2;
+  const C1C2R2RF2: f32 = Self::C1C2R2 * RF2;
+  const C2R2: f32 = C2 * R2;
+  const C1R1: f32 = C1 * R1;
+  const C1RF2: f32 = C1 * RF2;
+  const C1C2RF1RF2: f32 = Self::C1C2 * RF1 * RF2;
+  const C2RF1: f32 = C2 * RF1;
 
   pub fn new(sample_rate: f32) -> Self {
     Self {
@@ -31,48 +42,37 @@ impl TiltFilter {
   }
 
   fn get_s_domain_coefficients(&self, tilt: f32) -> ([f32; 3], [f32; 3]) {
-    let r_tilt_a = Self::R_TILT * tilt;
-    let r_tilt_b = Self::R_TILT * (1. - tilt);
+    let r_tilt_a = R_TILT * tilt;
+    let r_tilt_b = R_TILT * (1. - tilt);
 
-    let c1c2 = Self::C1 * Self::C2;
-    let c1c2r1 = c1c2 * Self::R1;
-    let c1c2r1r2 = c1c2r1 * Self::R2;
-    let c1c2r2 = c1c2 * Self::R2;
-    let c1c2r2rf2 = c1c2r2 * Self::RF2;
-    let c2r2 = Self::C2 * Self::R2;
-    let c1r1 = Self::C1 * Self::R1;
-    let c1rf2 = Self::C1 * Self::RF2;
-    let c1c2rf1rf2 = c1c2 * Self::RF1 * Self::RF2;
-    let c2rf1 = Self::C2 * Self::RF1;
-
-    let b0 = -c1c2r2rf2 * Self::RF1
-      + -c1c2r2rf2 * r_tilt_b
-      + -c1c2r1r2 * r_tilt_b
-      + -c1c2r2rf2 * Self::R1
-      + -c1c2rf1rf2 * r_tilt_a
-      + -c1c2r2rf2 * r_tilt_a;
-    let b1 = -c1rf2 * Self::RF1
-      + -c1rf2 * r_tilt_b
-      + -c2r2 * r_tilt_b
-      + -c2r2 * Self::RF2
-      + -c1r1 * r_tilt_b
-      + -c1r1 * Self::RF2
-      + -c1rf2 * r_tilt_a;
-    let b2 = -r_tilt_b + -Self::RF2;
-    let a0 = c1c2rf1rf2 * r_tilt_b
-      + c1c2r1 * Self::RF1 * r_tilt_b
-      + c1c2rf1rf2 * Self::R1
-      + c1c2r1r2 * Self::RF1
-      + c1c2r1 * Self::RF1 * r_tilt_a
-      + c1c2r1r2 * r_tilt_a;
-    let a1 = c2rf1 * r_tilt_b
-      + c2rf1 * Self::RF2
-      + c2r2 * Self::RF1
-      + c1r1 * Self::RF1
-      + c2rf1 * r_tilt_a
-      + c2r2 * r_tilt_a
-      + c1r1 * r_tilt_a;
-    let a2 = Self::RF1 + r_tilt_a;
+    let b0 = -Self::C1C2R2RF2 * RF1
+      - Self::C1C2R2RF2 * r_tilt_b
+      - Self::C1C2R1R2 * r_tilt_b
+      - Self::C1C2R2RF2 * R1
+      - Self::C1C2RF1RF2 * r_tilt_a
+      - Self::C1C2R2RF2 * r_tilt_a;
+    let b1 = -Self::C1RF2 * RF1
+      - Self::C1RF2 * r_tilt_b
+      - Self::C2R2 * r_tilt_b
+      - Self::C2R2 * RF2
+      - Self::C1R1 * r_tilt_b
+      - Self::C1R1 * RF2
+      - Self::C1RF2 * r_tilt_a;
+    let b2 = -r_tilt_b + -RF2;
+    let a0 = Self::C1C2RF1RF2 * r_tilt_b
+      + Self::C1C2R1 * RF1 * r_tilt_b
+      + Self::C1C2RF1RF2 * R1
+      + Self::C1C2R1R2 * RF1
+      + Self::C1C2R1 * RF1 * r_tilt_a
+      + Self::C1C2R1R2 * r_tilt_a;
+    let a1 = Self::C2RF1 * r_tilt_b
+      + Self::C2RF1 * RF2
+      + Self::C2R2 * RF1
+      + Self::C1R1 * RF1
+      + Self::C2RF1 * r_tilt_a
+      + Self::C2R2 * r_tilt_a
+      + Self::C1R1 * r_tilt_a;
+    let a2 = RF1 + r_tilt_a;
 
     ([b0, b1, b2], [a0, a1, a2])
   }
