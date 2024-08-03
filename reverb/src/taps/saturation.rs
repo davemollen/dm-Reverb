f818ac2e9bf02c7e19022225d2391f148a67f2dc
Dave Mollen <davemollen@gmail.com>
2024-08-03 15:57:38 +0200

    Use simd for saturation

--- reverb/src/taps/saturation.rs
+++ reverb/src/taps/saturation.rs
@@ -1,14 +1,17 @@
+use std::simd::{f32x4, num::SimdFloat};
+
 pub struct Saturation;
 
 impl Saturation {
-  pub fn process(input: f32, mix: f32) -> f32 {
-    let mix = mix.clamp(0., 1.);
-    input + (Self::fast_atan2(input) - input) * mix
+  pub fn process(taps: f32x4, average: f32) -> f32x4 {
+    let mix = f32x4::splat(average.clamp(0., 1.));
+    taps + (Self::fast_atan2(taps) - taps) * mix
   }
 
-  fn fast_atan2(x: f32) -> f32 {
-    let n1 = 0.97239411;
-    let n2 = -0.19194795;
-    ((n1 + n2 * x * x) * x).clamp(-1., 1.)
+  fn fast_atan2(x: f32x4) -> f32x4 {
+    let limit = f32x4::splat(1.);
+    let n1 = f32x4::splat(0.97239411);
+    let n2 = f32x4::splat(-0.19194795);
+    ((n1 + n2 * x * x) * x).simd_clamp(-limit, limit)
   }
 }
