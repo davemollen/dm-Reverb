bc3281dd1a948cc8599d478969004bb7d62d665c
Dave Mollen <davemollen@gmail.com>
2024-10-10 19:50:56 +0200

    Update dm-reverb.mk

--- .github/workflows/dm-reverb.mk
+++ .github/workflows/dm-reverb.mk
@@ -9,17 +9,14 @@ DM_REVERB_SITE = https://github.com/davemollen/dm-Reverb.git
 DM_REVERB_SITE_METHOD = git
 DM_REVERB_BUNDLES = dm-Reverb.lv2
 
-# Nightly toolchain is needed to enable simd
-define DM_REVERB_CONFIGURE_CMDS
-	~/.cargo/bin/rustup toolchain install nightly-2024-08-07
-	~/.cargo/bin/rustup default nightly-2024-08-07
-	~/.cargo/bin/rustup target add $(MOD_PLUGIN_BUILDER_RUST_TARGET)
-endef
-
 define DM_REVERB_BUILD_CMDS
+	~/.cargo/bin/rustup default nightly
+
 	rm -f $(@D)/lv2/dm-Reverb.lv2/libdm_reverb.so
 	(cd $(@D)/lv2 && \
 		~/.cargo/bin/cargo build $(MOD_PLUGIN_BUILDER_RUST_BUILD_FLAGS))
+
+	~/.cargo/bin/rustup default stable
 endef
 
 define DM_REVERB_INSTALL_TARGET_CMDS
